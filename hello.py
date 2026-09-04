95c01b857c0ccf122be8e8ad37f96c7a4e0c2203
chisaki0011 <senpai917@gmail>
2026-09-04 14:17:16 +0800

    First Post

---
+++ hello.py
@@ -0,0 +1,6 @@
+# printing statement cause this is the fundamental of python programming
+
+print("==============================================")
+print("Hello World")
+print("My first post!")
+print("==============================================")
